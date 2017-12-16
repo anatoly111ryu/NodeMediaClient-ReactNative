ec99030664abcc339e45cf73c354de59e5b7d150
Mingliang Chen <illuspas@gmail.com>
2017-12-16 08:10:03 +0800

    add demo

--- App.js
+++ App.js
@@ -9,49 +9,53 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { NodePlayerView } from 'react-native-nodemediaclient';
+import { NodeCameraView } from 'react-native-nodemediaclient';
 
 export default class App extends Component<{}> {
+  constructor() {
+    super();
+    this.state = { publishBtnTitle: 'Start Publish' };
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View >
+        <NodePlayerView
+          style={{ height: 200 }}
+          ref={(vp) => { this.vp = vp }}
+          inputUrl={"rtmp://192.168.0.10/live/stream"}
+          scaleMode={"ScaleAspectFit"}
+          bufferTime={300}
+          maxBufferTime={1000}
+          autoplay={true}
+        />
+        <NodeCameraView
+          style={{ height: 300 }}
+          ref={(vb) => { this.vb = vb }}
+          outputUrl={"rtmp://192.168.0.10/live/stream1"}
+          camera={{ cameraId: 1, cameraFrontMirror: true }}
+          audio={{ bitrate: 32000, profile: 1, samplerate: 44100 }}
+          video={{ preset: 12, bitrate: 400000, profile: 1, fps: 15, videoFrontMirror: false }}
+          autopreview={true}
+        />
+        <Button
+          onPress={() => {
+            if (this.state.isPublish) {
+              this.setState({ publishBtnTitle: 'Start Publish', isPublish: false });
+              this.vb.stop();
+            } else {
+              this.setState({ publishBtnTitle: 'Stop Publish', isPublish: true });
+              this.vb.start();
+            }
+          }}
+          title={this.state.publishBtnTitle}
+          color="#841584"
+        />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
